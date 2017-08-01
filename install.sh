d7d69a985d6ecaf964dab5bb6a0f2078dbb7fef4
Francis Labelle <francis.labelle22@gmail.com>
2017-08-01 18:13:50 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+cp getbrightness.sh /usr/bin/getbrightness
+cp setbrightness.sh /usr/bin/setbrightness
+cp maxbrightness.sh /usr/bin/maxbrightness
+
+chmod +x /usr/bin/getbrightness /usr/bin/setbrightness /usr/bin/maxbrightness
